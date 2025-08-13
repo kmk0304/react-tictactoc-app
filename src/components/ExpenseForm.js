b67b4294ba7b3d6c3a612cdf2c1dc0eb11d36f5b
kmk0304 <kmk0304@aladin.co.kr>
2025-08-13 16:17:07 +0900

    react-icons 사용

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import "./ExpenseForm.css"
+import { MdSend } from 'react-icons/md'
 
 export class ExpenseForm extends Component {
   render() {
@@ -29,6 +30,7 @@ export class ExpenseForm extends Component {
         </div>
         <button type= 'submit' className = 'btn'>
           제출
+          <MdSend className='btn-icon'/>
         </button>
       </form>
     )
